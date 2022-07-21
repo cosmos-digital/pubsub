1eef71b06cfbcdc33457f22d157c49dfeb5dd73a
Jef Kaefer <j.kaefer@swordhealth.com>
2022-07-21 07:52:16 +0100

    fix minors

--- subscriber/subscriber.go
+++ subscriber/subscriber.go
@@ -7,11 +7,11 @@ import (
 	pubsub "github.com/cosmos-digital/pubsub/internal"
 )
 
-func New(ctx context.Context, projectID string, topic string) (*pubsub.Publisher, error) {
+func New(ctx context.Context, projectID string) (*pubsub.Subscriber, error) {
 	connection := pubsub.New(projectID)
 	instance, err := connection.Connect(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create pubsub client: %w", err)
 	}
-	return instance.Publisher(topic), nil
+	return instance.Subscriber(), nil
 }
